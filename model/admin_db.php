8cac80b21ef1ae00405b00425e11c49fb1c43a31
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-05-04 19:29:12 -0500

    first commit

---
+++ model/admin_db.php
@@ -0,0 +1,32 @@
+<?php
+require('./model/database.php');
+
+function add_admin($username, $password) { 
+ global $db;
+ $hash = password_hash($password, PASSWORD_DEFAULT); 
+ $query = 'INSERT INTO administrators (username, password) 
+           VALUES (:username, :password)'; $statement = $db->prepare($query); 
+ $statement->bindValue(':username', $username); 
+ $statement->bindValue(':password', $hash); 
+ $statement->execute(); $statement->closeCursor();
+}
+
+function is_valid_admin_login($username, $password) { 
+ global $db;
+ $query = 'SELECT password FROM administrators 
+           WHERE username = :username'; 
+ $statement = $db->prepare($query); 
+ $statement->bindValue(':username', $username); 
+ $statement->execute(); 
+ $row = $statement->fetch(); 
+ $statement->closeCursor(); 
+ 
+ //check if null (ternary statement)
+ if(isset($_POST['submit'])) {
+  $password = isset($_POST['password']) ? $_POST['password'] : null;
+ } else {
+  $hash = $row['password'];
+ } 
+ return password_verify($password, $hash);  
+}
+?>
