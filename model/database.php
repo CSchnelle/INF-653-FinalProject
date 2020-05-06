4dc15a1d953a825ea46d8801001967987f15f660
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-05-06 13:39:29 -0500

    first commit

--- model/database.php
+++ model/database.php
@@ -5,9 +5,10 @@
     //$password = 'pa55word';
 
     // Heroku connection
-    $dsn = 'mysql:host=zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=f33okin59mld7tzf';
-    $username = 'hu0wlnc966uyzjy4';
-    $password = 'cmtbdgfv39s2i8o1';
+    $dsn = 'mysql://zvwj921x0g9kku90:o9wefx7cc59ztm8m@wp433upk59nnhpoh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ep1wrphu9699pdl3
+;dbname=ep1wrphu9699pdl3';
+    $username = 'zvwj921x0g9kku90';
+    $password = 'o9wefx7cc59ztm8m';
    
     try {
         //local development server connection
