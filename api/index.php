bd7bee5383659bde0a1d4afcaaba77aaa8b9022f
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-05-06 16:30:19 -0500

    second commit

--- api/index.php
+++ api/index.php
@@ -1,4 +1,15 @@
 <?php
+
+header('Access-Control-Allow-Origin:*');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+header('Access-Control-Allow-Methods: POST');
+
+include_once'./model/database.php';
+include_once './mode/quote_db.php';
+
+global $db;
+
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
 
     $authorID = trim(filter_input(INPUT_GET, 'authorID'));
