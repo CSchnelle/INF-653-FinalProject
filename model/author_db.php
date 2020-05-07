af85b8669036e91c75846f622dcf99e40a2db95f
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-05-06 20:21:03 -0500

    finale commit

--- model/author_db.php
+++ model/author_db.php
@@ -9,34 +9,22 @@
         return $authors;
     }
 
-    function get_author_name($author_id) {
+    function delete_author($authorID) {
         global $db;
-        $query = 'SELECT * FROM authors WHERE authorID = :author_id';
+        $query = 'DELETE FROM authors WHERE authorID = :authorID';
         $statement = $db->prepare($query);
-        $statement->bindValue(':author_id', $author_id);
-        $statement->execute();
-        $author = $statement->fetch();
-        $statement->closeCursor();
-        $author_name = $class['authorName'];
-        return $author_name;
-    }
-
-    function delete_author($author_id) {
-        global $db;
-        $query = 'DELETE FROM authors WHERE authorID = :author_id';
-        $statement = $db->prepare($query);
-        $statement->bindValue(':author_id', $author_id);
+        $statement->bindValue(':authorID', $authorID);
         $statement->execute();
         $statement->closeCursor();
     }
 
-    function add_author($author_name) {
+    function add_author($authorName) {
         global $db;
         $query = 'INSERT INTO authors (authorName)
               VALUES
                  (:authorName)';
         $statement = $db->prepare($query);
-        $statement->bindValue(':authorName', $author_name);
+        $statement->bindValue(':authorName', $authorName);
         $statement->execute();
         $statement->closeCursor();
     }
