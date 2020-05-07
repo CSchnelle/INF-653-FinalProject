04b360f1cd02217d1995eea5c0f3047ce2adeb21
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-05-06 20:27:42 -0500

    finale commit

--- index.php
+++ index.php
@@ -1,48 +1,42 @@
 <?php
     require('model/database.php');
+    require('model/quote_db.php');
+    require('model/quthor_db.php');
     require('model/categories_db.php');
-    require('model/author_db.php');
-    require('model/quotes_db.php');
 
-    $action = filter_input(INPUT_POST, 'action');
-    if ($action == NULL) {
-        $action = filter_input(INPUT_GET, 'action');
-        if ($action == NULL) {
-            $action = 'list_quotes';
-        }
-    } else {
-        
-        $action = 'list_quotes';
-    }
 
-    if ($action == 'list_quotes') {
-        $category_id = filter_input(INPUT_GET, 'category_id', FILTER_VALIDATE_INT);
-        $category_name = filter_input(INPUT_GET, 'category');
-        $sort = filter_input(INPUT_GET, 'sort');
+    $action = filter_input(INPUT_POST, 'action') ?? filter_input(INPUT_GET, 'action') ?? 'list_quotes';
 
-        
+    switch ($action) {
+        default: 
+            $authorID = filter_input(INPUT_GET, 'authorID', FILTER_VALIDATE_INT);
+            $categoryID = filter_input(INPUT_GET, 'categoryID', FILTER_VALIDATE_INT);
+            $sort = filter_input(INPUT_GET, 'sort');
 
-        $category_name = get_category_name($category_id);
+            $categoryName = get_category_name($categoryID);
+            $authorName = get_author_name($authorID);
 
-        $quotes = get_all_quotes($sort);
-        // apply author filter 
-        if ($author_name != NULL && $author_name != FALSE) {
-            $quotes = array_filter($quotes, function($array) use ($author_name) {
-                return $array["author"] == $author_name;
-            });
-        }
-        // apply category filter
-        if ($category_id != NULL && $category_id != FALSE) {
-            $quotes = array_filter($quotes, function($array) use ($category_name) {
-                return $array["categoryName"] == $category_name;
-            });
-        }
+            $quotes = get_all_quotes($sort);
+      
 
-        // use in drop menus 
-        $categories = get_categories();
-        $authors= get_authors();
-        include('view/header.php');
-        include('quote_list.php');
-        include('view/footer.php');
+            if (!empty($authorID)) {
+                $quotes = array_filter($quotes, function($array) use ($authorName) {
+                    return $array["authorName"] == $authorName;
+                });
+            }
+      
+            if (!empty($categoryID)) {
+                $quotes = array_filter($quotes, function($array) use ($categoryName) {
+                    return $array["categoryName"] == $categoryName;
+                });
+            }
+
+            // use in drop menus 
+            $aurhors = get_authors();
+            $categories = get_categories();
+            $quotes = get_quotes();
+            include('view/header.php');
+            include('quote_list.php');
+            include('view/footer.php');
     }
 ?> 
